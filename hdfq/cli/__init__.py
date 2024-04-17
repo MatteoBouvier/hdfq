3b01a0ec00e85ec2bb8488b0bf42b951c240205d
Matteo Bouvier <m.bouvier@vidium-solutions.com>
2024-04-17 23:24:52 +0200

    sepated main hdfq from hdfq+ (extra tools)

--- hdfq/cli/__init__.py
+++ hdfq/cli/__init__.py
@@ -1,3 +1,4 @@
 from hdfq.cli.app import app
+from hdfq.cli.tools import tools
 
-__all__ = ["app"]
+__all__ = ["app", "tools"]
--- hdfq/cli/app.py
+++ hdfq/cli/app.py
@@ -2,28 +2,25 @@ import sys
 from pathlib import Path
 from typing import Annotated, Optional, cast
 
-import ch5mpy as ch
 import click
 import tomllib
 import typer
 from typer.rich_utils import rich_format_error
 
 from hdfq.hdfq import run
-from hdfq.repair import repair_group
 
 app = typer.Typer(add_completion=False, pretty_exceptions_enable=False, no_args_is_help=True)
 
 
 def version_callback(value: bool):
     if value:
-        with open("pyproject.toml", mode="rb") as project_file:
+        with open(Path(__file__).parents[2] / "pyproject.toml", mode="rb") as project_file:
             project = tomllib.load(project_file)
             version = project["tool"]["poetry"]["version"]
         print(f"hdfq-{version}")
         raise typer.Exit()
 
 
-# @app.callback(invoke_without_command=True, no_args_is_help=True)
 @app.command(no_args_is_help=True)
 def main(
     ctx: typer.Context,
@@ -44,9 +41,6 @@ def main(
     It uses a syntax similar to jq's.
     See https://gitlab.vidium.fr/vidium/hdfq for documentation on supported filters.
     """
-    if filter == "repair":
-        return repair(ctx, path)
-
     if not path.exists():
         rich_format_error(click.UsageError(f"{path} does not exist for 'PATH'.", ctx=ctx))
 
@@ -55,40 +49,5 @@ def main(
     run(filter, path)
 
 
-@app.command(no_args_is_help=True)
-def repair(
-    ctx: typer.Context,
-    path: Annotated[
-        Path,
-        typer.Argument(
-            help="Path to a hdf5 file to repair",
-            show_default=False,
-        ),
-    ] = cast(Path, ... if sys.stdin.isatty() else Path(sys.stdin.read().strip())),
-    verbose: Annotated[bool, typer.Option("--verbose", "-v", help="verbose output")] = False,
-    in_RAM_copy: Annotated[bool, typer.Option("--in-RAM-copy", "-R", help="perform in-RAM copy of datasets")] = False,
-) -> None:
-    """
-    Repair corrupted HDF5 file by extracting valid groups and datasets.
-    """
-    if not path.exists():
-        rich_format_error(click.UsageError(f"{path} does not exist for 'PATH'.", ctx=ctx))
-
-        raise typer.Exit(code=1)
-
-    restore_path = path.with_stem("~" + path.stem)
-
-    with ch.File(path, mode=ch.H5Mode.READ) as corrupted_file, ch.File(
-        restore_path, mode=ch.H5Mode.WRITE_TRUNCATE
-    ) as new_file:
-        repair_group(corrupted_file, new_file, verbose, in_RAM_copy)
-
-    path.unlink()
-    restore_path.rename(path)
-
-
 if __name__ == "__main__":
-    commands = {"main", "repair"}
-    if sys.argv[1] not in commands:
-        sys.argv.insert(1, "main")
     app()
